720908f92597f80ce7df88eb62e8ad1128c3468c
Tonton <ton.email@example.com>
2025-07-10 18:54:19 +0200

    chore: add dummy sw.js to disable service worker

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+// Service Worker “Signature Tonton” : désenregistrement immédiat
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    self.registration.unregister().then(() =>
+      self.clients.matchAll().then(clients =>
+        clients.forEach(c => c.navigate(c.url))
+      )
+    )
+  );
+});
