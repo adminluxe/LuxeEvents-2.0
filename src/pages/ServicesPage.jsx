6e9e54a67e3fa68628ea7f1ee9657d4351ca56f6
Tonton <ton.email@example.com>
2025-07-18 02:57:30 +0200

    🧼 Uniformisation des pages + injection Layout global

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,7 +1,9 @@
+import Layout from "@/layouts/Layout";
 import React from 'react';
 
 export default function ServicesPage() {
   return (
+    <Layout>
     <div className="p-8 text-center">
       <h1 className="text-3xl sm:text-4xl font-bold text-center text-yellow-600 hover:scale-105 transition-transform duration-500 shadow-md hover:shadow-yellow-300 font-semibold text-yellow-600">Nos Services</h1>
       <p className="mt-4 text-gray-700">
